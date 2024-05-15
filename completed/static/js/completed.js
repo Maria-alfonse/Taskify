d0fd0512786b90734918c5688bf378cc5303d621
Maria-alfonse <mariaalfonse206@gmail.com>
2024-05-16 01:30:30 +0300

    7abit bgd

---
+++ completed/static/js/completed.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let completedTasks = JSON.parse(localStorage.getItem("completedTask")) || [];
+    const tasksList = document.getElementById("TasksList");
+  
+  });
+  
