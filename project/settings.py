2dccc5810f4bbaee08832d190aa9e7b345a641bc
ToqaAbdalla <132062106+Toqa781@users.noreply.github.com>
2024-05-15 19:56:48 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -31,8 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'tasks.apps.TasksConfig',
-    'addTask.apps.AddtaskConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
