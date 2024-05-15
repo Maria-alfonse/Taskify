ea73e0920aa146a3eb36644c5380e13fe4fdf1f8
ToqaAbdalla <132062106+Toqa781@users.noreply.github.com>
2024-05-15 19:55:54 +0300

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -5,6 +5,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('availableTasks.urls')),
-    path('AdminHome/', include('addTask.urls')),
-    path('',include('tasks.urls'))
+    
 ]
