f76b84ed0843901397f747dafb063661c8d23a53
maihassan1 <114602980+maihassan1@users.noreply.github.com>
2024-05-16 14:40:48 +0300

    Update forms.py

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,10 +5,7 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['Taskiid','title','teacher','choose','Description','admin','completed']
-
-        def __init__(self, *args, **kwargs):
-           super().__init__(*args, **kwargs)
-    # Disable the 'admin' field
-           self.fields['admin'].disabled = True
-
+        fields = ['Taskiid', 'title', 'teacher', 'choose', 'Description', 'admin', 'completed']
+        widgets = {
+            'admin': forms.TextInput(attrs={'readonly': True}),
+        }
