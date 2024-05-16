0dbd10e8178fd0927ca517f1f087915def203bc3
Toqa781 <abdallatoqa56@gmail.com>
2024-05-16 14:54:01 +0300

    my urlssss

--- project/urls.py
+++ project/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
-   
+    path('',include('availableTasks.urls')),
     path('addTask/', include('addTask.urls')),
     path('Home/',include('tasks.urls')),
    
