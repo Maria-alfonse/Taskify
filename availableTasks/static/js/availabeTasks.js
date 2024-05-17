ea0eca894b67c231062bb148ef259553bf46d3a1
Toqa781 <abdallatoqa56@gmail.com>
2024-05-17 23:02:26 +0300

    priority for available

--- availableTasks/static/js/availabeTasks.js
+++ availableTasks/static/js/availabeTasks.js
@@ -1,20 +1,21 @@
-var x = document.getElementById("high");
-var y = document.getElementById("medium");
-var z = document.getElementById("low");
-        function high() {
+var x = document.getElementById("upcom");
+var y = document.getElementById("ovrdu");
+var z = document.getElementById("cmplt");
+        function upcoming() {
             x.style.left = "15px";
             y.style.left = "900px";
             z.style.left = "1200px";
         }
 
-        function medium() {
+        function overdue() {
             x.style.left = "-1185px";
             y.style.left = "15px";
             z.style.left = "900px"
         }
-        function low() {
+        function completed() {
             x.style.left = "-2090px";
             y.style.left = "-900px";
             z.style.left="15px"
         }
-    
+        document.getElementById("id").innerHTML=localStorage.getItem("TtitleVal");
+        document.getElementById("user").innerHTML=localStorage.getItem("username");
