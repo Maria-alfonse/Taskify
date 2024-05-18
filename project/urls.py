137f052f3b9ba764412277dc553f572abf0b5670
Habiba775 <125930000+Habiba775@users.noreply.github.com>
2024-05-19 00:58:09 +0300

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -1,11 +1,15 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from myapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('availableTasks.urls')),
     path('completed/', include('completed.urls')),
     path('addTask/', include('addTask.urls')),
     path('Home/',include('tasks.urls')),
-   
+       
+   path('', include('myapp.urls')),
 ]
