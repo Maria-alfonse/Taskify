6ef3a85646aadc59fa53efb00fb26b3ec06b4ff5
Maria-alfonse <125464100+Maria-alfonse@users.noreply.github.com>
2024-07-07 23:02:17 +0300

    Delete viewlist directory

--- viewlist/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns= [
-    path('viewlist/',views.viewlist, name='list'),
-    path('tasks/<int:taskid>/', views.task_detail, name='task_detail'),
-]
